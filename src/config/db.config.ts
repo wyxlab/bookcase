b7e5edd1f103b926e670a2b3a3d293af642a92a9
Weian Xu <weian.wyxlab@gmail.com>
2024-08-18 22:50:29 +0200

    First db connection attempt, failed

---
+++ src/config/db.config.ts
@@ -0,0 +1,6 @@
+export const Conn = {
+    host: 'localhost',
+    user: 'me',
+    password: 'secret',
+    database: 'my_db'
+}
---
+++ src/db/Connection.ts
@@ -0,0 +1,17 @@
+import { mysql } from "mysql"
+import { Conn } from "../config/db.config"
+
+function createRawConnection() {
+    const connection = mysql.createConnection(Conn)
+    console.log("Connection to db...")
+    connection.connect(function(err) {
+        if (err) {
+          console.error('error connecting: ' + err.stack);
+          return;
+        }
+      
+        console.log('connected as id ' + connection.threadId);
+      })
+
+      return connection;
+}
